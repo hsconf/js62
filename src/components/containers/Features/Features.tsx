89b9823fe030be9070da36b31041174ac21e74ca
Akylbek <hsplay74@gmail.com>
2024-10-21 18:41:47 +0600

    features page v1

---
+++ src/components/containers/Features/Features.tsx
@@ -0,0 +1,10 @@
+
+const Features = () => {
+    return (
+        <div>
+            all Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, et.
+        </div>
+    );
+};
+
+export default Features;
